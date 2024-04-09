4058c274c23cb2fb759e5d5928ecf5d713444cc3
dir-FY24izumitakuma <takuma.izumi.l696@dirbato.co.jp>
2024-04-09 15:09:03 +0900

    最初のコミット

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -4,7 +4,10 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello,World");
-        System.out.println("こんにちは、世界！");
+        System.out.println("こんにちは、世界！");   
+        
+        String str = " Hello wo ";
+        System.out.println(str.length()); 
     }
 
 }
